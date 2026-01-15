5572954d20526970637d3c4ca1cfee8e6e5f37e5
Kevin Danae Aguilera Barragan <48744821+Kevin-Danaea@users.noreply.github.com>
2026-01-15 13:17:18 -0500

    Revert "Feat/deeplinks"

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 
-import { NextConfig } from 'next';
+import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
-
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/.well-known/apple-app-site-association',
-        headers: [{ key: 'Content-Type', value: 'application/json' }],
-      },
-      {
-        source: '/.well-known/assetlinks.json',
-        headers: [{ key: 'Content-Type', value: 'application/json' }],
-      },
-    ];
-  },
-};
-
+ 
+const nextConfig: NextConfig = {};
+ 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
